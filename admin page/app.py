05bb76aa5ef64239d1eb90930595b8c7bcea08ca
maal03 <167853368+maal03@users.noreply.github.com>
2024-05-14 11:11:59 +0500

    Update app.py

--- admin page/app.py
+++ admin page/app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 table_data = [
     ["AIRASIA", "A10023", "20:00-22:00", "READY"],
-    ["AIRASIA", "A12233", "13:00-19:00", "NOT READY"],
-    ["AIRASIA", "A45677", "12:00-3:00", "NOT READY"],
-    ["AIRASIA", "A23456", "14:00-15:00", "READY"],
-    ["AIRASIA", "A87654", "9:00-13:00", "NOT READY"],
-    ["AIRASIA", "A45687", "1:00-4:00", "NOT READY"],
-    ["AIRASIA", "A96753", "13:00-16:00", "NOT READY"],
-    ["AIRASIA", "A16662", "18:00-20:00", "READY"],
-    ["AIRASIA", "A09876", "7:00-10:00", "READY"]
+    ["China Southern Airlines", "A12233", "13:00-19:00", "NOT READY"],
+    ["Emirates", "A45677", "12:00-3:00", "NOT READY"],
+    ["Singapore Airlines", "A23456", "14:00-15:00", "READY"],
+    ["Vietnam Airlines", "A87654", "9:00-13:00", "NOT READY"],
+    ["Qatar Airways", "A45687", "1:00-4:00", "NOT READY"],
+    ["Malaysia Airlines", "A96753", "13:00-16:00", "NOT READY"],
+    ["Sri Lankan Airlines ", "A16662", "18:00-20:00", "READY"],
+    ["Bangkok Airways", "A09876", "7:00-10:00", "READY"]
 ]
 
 @app.route('/')
